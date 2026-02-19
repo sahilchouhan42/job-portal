8d38a1eb057afb8f5535c8771ad53a5334273a26
Sahil Chouhan <chouhangolu604@gmail.com>
2026-02-19 12:46:16 +0530

    upate in controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,16 +1,17 @@
 import User from "../models/user.model.js"
 import ApiResponse from "../utils/api.response.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 
-export const register = async (req, res)=>{
+export const register = async (req, res) => {
     try {
-        const {fullName, email, phoneNumber, password, role} = req.body
-        if(!fullName||!email||!phoneNumber||!password||!role) return res.status(400).json(new ApiResponse(400, "All Fields are required"))
-        
+        const { fullName, email, phoneNumber, password, role } = req.body
+        if (!fullName || !email || !phoneNumber || !password || !role) return res.status(400).json(new ApiResponse(400, "All Fields are required"))
+
         //check exist user
-        const existUser = await User.findOne({email})
-        if(existUser) return res.status(409).json(new ApiResponse(409, "User alreday exist"))
+        const existUser = await User.findOne({ email })
+        if (existUser) return res.status(409).json(new ApiResponse(409, "User alreday exist"))
 
         //hashing password
         const hashedPassword = await bcrypt.hash(password, 10)
@@ -30,3 +31,39 @@ export const register = async (req, res)=>{
     }
 }
 
+export const login = async (req, res) => {
+    try {
+        const { email, password, role } = req.body
+        if (!email || !password || !role) {
+            return res.status(400).json(new ApiResponse(400, "All fields required"))
+        }
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json(new ApiResponse(400, "User does not exist"))
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        if (!isPasswordMatch) return res.status(400).json(new ApiResponse(400, "Invalid Credentials"))
+
+        //check role is correct or not
+        if (role !== user.role) return res.status(400).json(new ApiResponse(400, "You are logging with different role"))
+
+        //token generateion
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE })
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json(new ApiResponse(200, "Login Successfull", user))
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json(new ApiResponse(500, "Internal Server Error"))
+    }
+}
+
+export const logout = async (req, res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json(new ApiResponse(500, "Internal Server Error"))
+    }
+}
